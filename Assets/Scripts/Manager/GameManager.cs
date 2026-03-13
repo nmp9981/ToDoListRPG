5d2aa3c71f684ae3c75f87e1afd88010455fc9d2
tybnasgo <tybnasgo@naver.com>
2026-03-14 01:03:27 +0900

    Update gitignore and clean up files

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _instance;
+    public static GameManager Instance { get { Init(); return _instance; } }
+    
+    static void Init()
+    {
+        if (_instance == null)
+        {
+            GameObject gm = GameObject.Find("GameManager");
+            if (gm == null)
+            {
+                gm = new GameObject { name = "GameManager" };
+
+                gm.AddComponent<GameManager>();
+            }
+            DontDestroyOnLoad(gm);
+            _instance = gm.GetComponent<GameManager>();
+        }
+    }
+
+}
