86b557ac9f2bb88690abd1bf146a5d17cdffc243
tybnasgo <tybnasgo@naver.com>
2026-04-09 22:52:14 +0900

    허용 프로그램 UI구현

--- Assets/Scripts/UI/SettingUI.cs
+++ Assets/Scripts/UI/SettingUI.cs
@@ -5,6 +5,7 @@ public class SettingUI : MonoBehaviour
 {
     [SerializeField] private Image _concentrationImage;//집중 이미지
     [SerializeField] private Sprite[] _concentrationImageSet = new Sprite[2];//집중 이미지
+    [SerializeField] GameObject _programSetObj;//프로그램 세팅 오브젝트
 
     /// <summary>
     /// 일반 모드로 변경
@@ -18,5 +19,13 @@ public class SettingUI : MonoBehaviour
     {
         GameManager.Instance.PlayMode = PlayMode.Concentration;
         _concentrationImage.sprite = _concentrationImageSet[1];
+    }
+    public void OpenProgramSetUI()
+    {
+        _programSetObj.SetActive(true);
+    }
+    public void CloseProgramSetUI()
+    {
+        _programSetObj.SetActive(false);
     }
 }
